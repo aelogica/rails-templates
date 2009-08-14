aeb8e7196bd7fdb39ca9c42947ac64a03072656b
Dr Nic Williams <drnicwilliams@gmail.com>
2009-08-15 08:56:43 +1000

    renamed + rewired template spec for old slicehost template

--- spec/mocra/slicehost_template_spec.rb
+++ spec/mocra/slicehost_template_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "template_runner" do
   before(:each) do
-    setup_template_runner
+    setup_template_runner "mocra-slicehost.rb"
   end
   describe "slicehost" do
     before(:each) do
--- spec/mocra/spec_helper.rb
+++ spec/mocra/spec_helper.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
-def setup_template_runner
+def setup_template_runner(template_name = "mocra.rb")
   ENV['TEST_MODE'] = '1'
-  @template = File.dirname(__FILE__) + "/../../mocra.rb"
+  @template = File.dirname(__FILE__) + "/../../#{template_name}"
   @runner = Rails::TemplateRunner.new(@template)
 end
