873d7a2c0a3605f8cb21fbb1b274d2cd78346a67
Dr Nic Williams <drnicwilliams@gmail.com>
2009-09-25 09:28:39 +1000

    default robots.txt now tells google to bugger off

--- mocra.rb
+++ mocra.rb
@@ -81,11 +81,15 @@ template do
   run "rm README"
   run "rm public/index.html"
   run "rm public/favicon.ico"
-  run "rm public/robots.txt"
   run "rm -f public/javascripts/*"
   run "rm -rf test"
 
   file "README.md", ""
+  
+  file "public/robots.txt", <<-EOS.gsub(/^  /, '')
+  User-agent: *
+  Disallow: /
+  EOS
   
   if ENV['DB'] == "mysql"
     file 'config/database.yml', <<-EOS.gsub(/^  /, '')
@@ -231,10 +235,18 @@ end
   # Use 'Ruby Machinst.tmbundle' Cmd+B to generate blueprints from class names
   require "faker"
 
-  Sham.name  { Faker::Name.name }
-  Sham.login { Faker::Internet.user_name.gsub(/\W/, '')[0..14] } # max 15 chars
-  Sham.message { Faker::Lorem.sentence }
+  Sham.define do
+    name              { Faker::Name.name }
+    first_name        { Faker::Name.first_name }
+    last_name         { Faker::Name.last_name }
+    company_name      { Faker::Company.name }
+    login             { Faker::Internet.user_name.gsub(/\W/, '')[0..14] } # max 15 chars
+    message           { Faker::Lorem.sentence }
+    description       { Faker::Lorem.sentence }
+    email             { Faker::Internet.email }
+  end
   
+  Dir[File.join(File.dirname(__FILE__), 'blueprints', '*_blueprint.rb')].each {|bp| require bp}
   EOS
 
 # Initial controllers/views
