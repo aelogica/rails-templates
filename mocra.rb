eacde5f7d32305e293b676dfaf82f34cb25b2437
Dr Nic Williams <drnicwilliams@gmail.com>
2009-11-20 11:19:53 +1000

    Adding authlogic support. Removing restful_authentication support.

--- mocra.rb
+++ mocra.rb
@@ -53,12 +53,12 @@ template do
   end
   
 # Authentication selection
-  auth = highline.choose(*%w[none restful_authentication twitter_auth]) do |menu|
+  auth = highline.choose(*%w[none authlogic twitter_auth]) do |menu|
     menu.prompt = "Which user authentication system?  "
   end
-  authentication         = auth != "none"
-  twitter_auth           = auth == "twitter_auth"
-  restful_authentication = auth == "restful_authentication"
+  authentication           = auth != "none"
+  twitter_auth             = auth == "twitter_auth"
+  authlogic = auth == "authlogic"
 
 # Setup twitter oauth on twitter.com
   if twitter_auth
@@ -86,9 +86,9 @@ template do
 
   file "README.md", ""
   
-  file "public/robots.txt", <<-EOS.gsub(/^  /, '')
-  User-agent: *
-  Disallow: /
+  file "public/robots.txt", <<-EOS.gsub(/^    /, '')
+    User-agent: *
+    Disallow: /
   EOS
   
   if ENV['DB'] == "mysql"
@@ -129,13 +129,13 @@ template do
 # Set up .gitignore files
   run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
   run %{find . -type d -empty | grep -v "vendor" | grep -v ".git" | grep -v "tmp" | xargs -I xxx touch xxx/.gitignore}
-  file '.gitignore', <<-EOS.gsub(/^  /, '')
-  .DS_Store
-  log/*.log
-  tmp/**/*
-  config/database.yml
-  config/initializers/site_keys.rb
-  db/*.sqlite3
+  file '.gitignore', <<-EOS.gsub(/^    /, '')
+    .DS_Store
+    log/*.log
+    tmp/**/*
+    config/database.yml
+    config/initializers/site_keys.rb
+    db/*.sqlite3
   EOS
 
 # Set up git repository and commit all work so far to the repository
@@ -145,11 +145,11 @@ template do
 
 # Authentication gems/plugins
 
-if twitter_auth
-  plugin 'twitter_auth', :git => 'git://github.com/mbleigh/twitter-auth.git'
-elsif restful_authentication
-  plugin 'restful_authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
-end
+  if twitter_auth
+    plugin 'twitter_auth', :git => 'git://github.com/mbleigh/twitter-auth.git'
+  elsif authlogic
+    heroku_gem 'authlogic'
+  end
 
 
 # Set up session store initializer
@@ -217,84 +217,128 @@ end
     heroku_gem 'ezcrypto'
     heroku_gem 'oauth'
     generate 'twitter_auth', '--oauth'
-  elsif restful_authentication
-    generate 'authenticated', 'user session --include-activation --rspec'
+  elsif authlogic
+    generate 'session', 'user_session'
+    generate 'rspec_model', 'User'
+    create_users_file = Dir['db/migrate/*_create_users.rb'].first
+    file create_users_file, <<-EOS.gsub(/^      /, '')
+      class CreateUsers < ActiveRecord::Migration
+        def self.up
+          create_table :users do |t|
+            t.string    :login,               :null => false                # optional, you can use email instead, or both
+            t.string    :email,               :null => false                # optional, you can use login instead, or both
+            t.string    :crypted_password,    :null => false                # optional, see below
+            t.string    :password_salt,       :null => false                # optional, but highly recommended
+            t.string    :persistence_token,   :null => false                # required
+            t.string    :single_access_token, :null => false                # optional, see Authlogic::Session::Params
+            t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
+
+            # Magic columns, just like ActiveRecord's created_at and updated_at. These are automatically maintained by Authlogic if they are present.
+            t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+            t.integer   :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+            t.datetime  :last_request_at                                    # optional, see Authlogic::Session::MagicColumns
+            t.datetime  :current_login_at                                   # optional, see Authlogic::Session::MagicColumns
+            t.datetime  :last_login_at                                      # optional, see Authlogic::Session::MagicColumns
+            t.string    :current_login_ip                                   # optional, see Authlogic::Session::MagicColumns
+            t.string    :last_login_ip                                      # optional, see Authlogic::Session::MagicColumns
+
+            t.timestamps
+          end
+
+          add_index :users, :email
+          add_index :users, :login, :unique => true
+        end
+
+        def self.down
+          drop_table :users
+        end
+      end
+    EOS
+    file 'app/models/user.rb', <<-EOS.gsub(/^        /, '')
+      class User < ActiveRecord::Base
+        acts_as_authentic
+      end
+    EOS
   end
   
-  file 'public/stylesheets/form.css', <<-CSS.gsub(/^  /, '')
-  /* Decent styling of formtastic forms */
-  form fieldset {
-    border: 0;
-  }
+  file 'public/stylesheets/form.css', <<-CSS.gsub(/^    /, '')
+    /* Decent styling of formtastic forms */
+    form fieldset {
+      border: 0;
+    }
 
-  form li {
-    list-style-type: none;
-  }
+    form li {
+      list-style-type: none;
+    }
 
-  form li {
-    margin: 0 0 0.5em;
-    clear: both;
-  }
+    form li {
+      margin: 0 0 0.5em;
+      clear: both;
+    }
 
-  form label {
-    display: block;
-    text-align: right;
-    width: 150px;
-    float: left;
-    position: relative;
-    top: 6px;
-    padding-right: 6px;
-  }
+    form label {
+      display: block;
+      text-align: right;
+      width: 150px;
+      float: left;
+      position: relative;
+      top: 6px;
+      padding-right: 6px;
+    }
 
-  form .inline-errors {
-    display: inline;
-    color: red;
-  }
+    form .inline-errors {
+      display: inline;
+      color: red;
+    }
   CSS
   
-  append_file 'features/support/env.rb', <<-EOS.gsub(/^  /, '')
-  require "email_spec/cucumber"
-  require File.dirname(__FILE__) + "/../../spec/blueprints"
+  file 'features/support/env_extn.rb', <<-EOS.gsub(/^    /, '')
+    require "email_spec/cucumber"
+    require File.dirname(__FILE__) + "/../../spec/blueprints"
 
-  Before do
-    FakeWeb.allow_net_connect = false
-  end
-  EOS
-
-  append_file 'spec/spec_helper.rb', <<-EOS.gsub(/^  /, '')
-  require File.dirname(__FILE__) + '/blueprints'
-  
-  # When running specs in TextMate, provide an rputs method to cleanly print objects into HTML display
-  # From http://talklikeaduck.denhaven2.com/2009/09/23/rspec-textmate-pro-tip
-  module Kernel
-    if ENV.keys.find {|env_var| env_var.start_with?("TM_")}
-      def rputs(*args)
-        puts( *["<pre>", args.collect {|a| CGI.escapeHTML(a.to_s)}, "</pre>"])
-      end
-    else
-      alias_method :rputs, :puts
+    Before do
+      FakeWeb.allow_net_connect = false
     end
-  end
+  EOS
+
+  append_file 'spec/spec_helper.rb', <<-EOS.gsub(/^    /, '')
+    require File.dirname(__FILE__) + '/blueprints'
+  
+    # When running specs in TextMate, provide an rputs method to cleanly print objects into HTML display
+    # From http://talklikeaduck.denhaven2.com/2009/09/23/rspec-textmate-pro-tip
+    module Kernel
+      if ENV.keys.find {|env_var| env_var.start_with?("TM_")}
+        def rputs(*args)
+          puts( *["<pre>", args.collect {|a| CGI.escapeHTML(a.to_s)}, "</pre>"])
+        end
+        def rp(*args)
+          puts( *["<pre>", args.collect {|a| CGI.escapeHTML(a.inspect)}, "</pre>"])
+        end
+      else
+        alias_method :rputs, :puts
+        alias_method :rp, :p
+      end
+    end
   EOS
   
-  file 'spec/blueprints.rb', <<-EOS.gsub(/^  /, '')
-  # Use 'Ruby Machinst.tmbundle' Cmd+B to generate blueprints from class names
-  require 'machinist/active_record'
-  require 'sham'
-  require 'faker'
+  file 'spec/blueprints.rb', <<-EOS.gsub(/^    /, '')
+    # Use 'Ruby Machinst.tmbundle' Cmd+B to generate blueprints from class names
+    require 'machinist/active_record'
+    require 'sham'
+    require 'faker'
 
-  Sham.define do
-    name              { Faker::Name.name }
-    first_name        { Faker::Name.first_name }
-    last_name         { Faker::Name.last_name }
-    company_name      { Faker::Company.name }
-    login             { Faker::Internet.user_name.gsub(/\W/, '')[0..14] } # max 15 chars
-    message           { Faker::Lorem.sentence }
-    description       { Faker::Lorem.sentence }
-    email             { Faker::Internet.email }
-  end
+    Sham.define do
+      name              { Faker::Name.name }
+      first_name        { Faker::Name.first_name }
+      last_name         { Faker::Name.last_name }
+      company_name      { Faker::Company.name }
+      login             { Faker::Internet.user_name.gsub(/\W/, '')[0..14] } # max 15 chars
+      message           { Faker::Lorem.sentence }
+      description       { Faker::Lorem.sentence }
+      email             { Faker::Internet.email }
+    end
   
-  Dir[File.join(File.dirname(__FILE__), 'blueprints', '*_blueprint.rb')].each {|bp| require bp}
+    Dir[File.join(File.dirname(__FILE__), 'blueprints', '*_blueprint.rb')].each {|bp| require bp}
   EOS
 
 # Initial controllers/views
@@ -303,29 +347,67 @@ end
   if authentication
     generate 'rspec_controller', 'protected index'
 
-    file 'app/controllers/protected_controller.rb', <<-EOS.gsub(/^  /, '')
-    class ProtectedController < ApplicationController
-      before_filter :login_required
+    file 'app/controllers/protected_controller.rb', <<-EOS.gsub(/^      /, '')
+      class ProtectedController < ApplicationController
+        before_filter :login_required
 
-      def index
+        def index
+        end
+
       end
-
-    end
     EOS
 
     file 'app/views/protected/index.html.haml', '%h3= current_user.login'
   end
   
-  if twitter_auth
-    file 'app/views/home/index.html.erb', <<-EOS.gsub(/^    /, '')
-    <%= link_to "Protected", :controller => :protected %>
+  if authlogic
+    gsub_file 'app/controllers/application_controller.rb', /^end\Z/m do
+      <<-EOS.gsub(/^        /, '')
+          private
+            def current_user_session
+              return @current_user_session if defined?(@current_user_session)
+              @current_user_session = UserSession.find
+            end
 
-    <h3>Recent users (<%= User.count %>)</h3>
-    <ul>
-    <% for user in User.all -%>
-      <li><%= image_tag(user.profile_image_url) %><%= user.name %> (<%= link_to h(user.login), "http://twitter.com/\#{h user.login}" %>)</li>
-    <% end -%>
-    </ul>
+            def current_user
+              return @current_user if defined?(@current_user)
+              @current_user = current_user_session && current_user_session.user
+            end
+
+            def login_required
+              unless current_user
+                if current_user_session && current_user_session.stale?
+                  flash[:notice] = "Your session has been logged out automatically"
+                else
+                  flash[:error] = "You must be logged in to access this page"
+                end
+
+                store_location
+                redirect_to new_user_session_url
+                return false
+              end
+            end
+        end
+      EOS
+    end
+  end
+  
+  if twitter_auth
+    file 'app/views/home/index.html.erb', <<-EOS.gsub(/^      /, '')
+      <%= link_to "Protected", :controller => :protected %>
+
+      <h3>Recent users (<%= User.count %>)</h3>
+      <ul>
+      <% for user in User.all -%>
+        <li><%= image_tag(user.profile_image_url) %><%= user.name %> (<%= link_to h(user.login), "http://twitter.com/\#{h user.login}" %>)</li>
+      <% end -%>
+      </ul>
+    EOS
+  elsif authentication
+    file 'app/views/home/index.html.haml', <<-EOS.gsub(/^      /, '')
+      %h1 Welcome!
+      %p
+        = link_to "Protected", :controller => :protected
     EOS
   else
     file 'app/views/home/index.html.haml', '%h1 Welcome!'
@@ -336,52 +418,45 @@ end
     append_file 'config/environments/development.rb', "\n\nOpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE\n"
     append_file 'config/environments/test.rb', "\n\nOpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE\n"
     
-    file 'config/twitter_auth.yml', <<-EOS.gsub(/^    /, '')
-    development:
-      strategy: oauth
-      oauth_consumer_key: #{twitter_auth_keys[:key]}
-      oauth_consumer_secret: #{twitter_auth_keys[:secret]}
-      base_url: "https://twitter.com"
-      api_timeout: 10
-      remember_for: 14 # days
-      oauth_callback: "http://#{app_subdomain}.local/oauth_callback"
-    test:
-      strategy: oauth
-      oauth_consumer_key: #{twitter_auth_keys[:key]}
-      oauth_consumer_secret: #{twitter_auth_keys[:secret]}
-      base_url: "https://twitter.com"
-      api_timeout: 10
-      remember_for: 14 # days
-      oauth_callback: "http://#{app_subdomain}.local/oauth_callback"
-    production:
-      strategy: oauth
-      oauth_consumer_key: #{twitter_auth_keys[:key]}
-      oauth_consumer_secret: #{twitter_auth_keys[:secret]}
-      base_url: "https://twitter.com"
-      api_timeout: 10
-      remember_for: 14 # days
-      oauth_callback: "http://#{app_url}/oauth_callback"
+    file 'config/twitter_auth.yml', <<-EOS.gsub(/^      /, '')
+      development:
+        strategy: oauth
+        oauth_consumer_key: #{twitter_auth_keys[:key]}
+        oauth_consumer_secret: #{twitter_auth_keys[:secret]}
+        base_url: "https://twitter.com"
+        api_timeout: 10
+        remember_for: 14 # days
+        oauth_callback: "http://#{app_subdomain}.local/oauth_callback"
+      test:
+        strategy: oauth
+        oauth_consumer_key: #{twitter_auth_keys[:key]}
+        oauth_consumer_secret: #{twitter_auth_keys[:secret]}
+        base_url: "https://twitter.com"
+        api_timeout: 10
+        remember_for: 14 # days
+        oauth_callback: "http://#{app_subdomain}.local/oauth_callback"
+      production:
+        strategy: oauth
+        oauth_consumer_key: #{twitter_auth_keys[:key]}
+        oauth_consumer_secret: #{twitter_auth_keys[:secret]}
+        base_url: "https://twitter.com"
+        api_timeout: 10
+        remember_for: 14 # days
+        oauth_callback: "http://#{app_url}/oauth_callback"
     EOS
-  elsif restful_authentication
-    environment("config.active_record.observers = :user_observer")
-    initializer("mailer.rb", <<-EOS.gsub(/^    /, ''))
+  end
+
+  initializer "mailer.rb", <<-EOS.gsub(/^    /, '')
     mailer_options = YAML.load_file("\#{RAILS_ROOT}/config/mailer.yml")
     ActionMailer::Base.smtp_settings = mailer_options
-    EOS
-    file("config/mailer.yml", <<-EOS.gsub(/^    /, ''))
+  EOS
+  file "config/mailer.yml", <<-EOS.gsub(/^    /, '')
     :address: mail.authsmtp.com
     :domain: #{domain}
     :authentication: :login
     :user_name: USERNAME
     :password: PASSWORD
-    EOS
-    append_file("app/views/users/new.html.erb", <<-EOS.gsub(/^    /, ''))
-    <h2>FIRST - setup activation config/mailer.yml for your mail server</h2>
-    EOS
-    gsub_file("app/controllers/application_controller.rb", /class ApplicationController < ActionController::Base/mi) do
-      "class ApplicationController < ActionController::Base\n  include AuthenticatedSystem"
-    end
-  end
+  EOS
 
 # Run migrations
   rake 'db:migrate'
@@ -393,10 +468,8 @@ end
     route "map.session_create  '/sessions/create',  :controller => 'session', :action => 'create'"
     route "map.session_destroy  '/sessions/destroy',  :controller => 'session', :action => 'destroy'"
     route "map.oauth_callback  '/oauth_callback',  :controller => 'session', :action => 'oauth_callback'"
-    
-  elsif restful_authentication
-    # restful-authentication seems to create other routes but not this one
-    route "map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil"
+  elsif authlogic
+    route "map.resource :user_session"
   end
   
   route "map.root :controller => 'home', :action => 'index'"
